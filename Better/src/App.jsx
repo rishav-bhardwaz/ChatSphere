5c909c696363c324452ef98656995e00393648d5
rishav-bhardwaz <rishav76000@gmail.com>
2024-10-13 14:51:26 +0530

    Need Css

--- Better/src/App.jsx
+++ Better/src/App.jsx
@@ -4,7 +4,6 @@ import Main from "./components/Main/Main";
 const App = () => {
   return (
     <>
-
       <Main />
     </>
   );
--- Better/src/components/Main/Main.jsx
+++ Better/src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
+import "./main.css";
 
 const Main = () => {
   const {
