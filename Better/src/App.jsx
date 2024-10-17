c5bee23d0f7667a1ea0a78243c8da415586674e4
rishav-bhardwaz <rishav76000@gmail.com>
2024-10-17 20:31:27 +0530

    sidebar with glitches

--- Better/src/App.jsx
+++ Better/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Main from "./components/Main/Main";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
     <>
       <Main />
+      <Sidebar />
     </>
   );
 };
